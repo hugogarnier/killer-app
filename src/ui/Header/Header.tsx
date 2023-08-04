1df108bc2ae0cc5102b587d0625ac1621e71a598
Hugo Garnier <80961204+hugogarnier@users.noreply.github.com>
2023-08-04 20:48:22 +0200

    adjust top inset

--- src/ui/Header/Header.tsx
+++ src/ui/Header/Header.tsx
@@ -1,6 +1,8 @@
 import React, { FC } from 'react';
 import { Platform, View } from 'react-native';
 
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
 import { Primary } from './components/Primary';
 import { Secondary } from './components/Secondary';
 import { Tertiary } from './components/Tertiary';
@@ -12,12 +14,13 @@ type HeaderProps = {
 };
 
 export const Header: FC<HeaderProps> = ({ variant = 'primary', title, onPress }) => {
+  const { top } = useSafeAreaInsets();
   const isAndroid = Platform.OS === 'android';
 
   return (
     <View
       className={`flex flex-row justify-between items-center w-full px-6 pb-4 overflow-hidden ${
-        (isAndroid && 'pt-10') || 'pt-8'
+        (isAndroid && top && 'pt-14') || 'pt-10'
       }`}
     >
       {variant === 'primary' && <Primary />}
